5371d670f9ed747fee34571785a5a133f3892e45
Richard Capraro <richard.capraro@gmail.com>
2022-05-28 21:47:12 +0200

    Add more variance samples

--- src/main/scala/fr/brl/tips/variance/AnimalVariance.scala
+++ src/main/scala/fr/brl/tips/variance/AnimalVariance.scala
@@ -31,30 +31,30 @@ object AnimalVariance extends App {
 
   val xCage: XCage[Cat] = new XCage[Animal]
 
-  //<editor-fold desc="Variance positions demystified">
+  // <editor-fold desc="Variance positions demystified">
 
-  class InvariantCage[T](val animal: T) // invariant POSITION
+  class InvariantCage[T](val animal: T) // INVARIANT POSITION - OK
 
   // covariant positions
-  class CovariantCage[+T](val animal: T) // COVARIANT POSITION
+  class CovariantCage[+T](val animal: T) // COVARIANT POSITION - OK
 
   // class ContravariantCage[-T](val animal: T) // error: CONTRAVARIANT type T occurs in COVARIANT position
-  // because types of vars are in CONTRAVARIANT POSITION
+  // because types of vars are here in CONTRAVARIANT POSITION
 
   // without this compilation error, it would be possible to write:
   // val catCage: ContravariantCage[Cat] = new ContravariantCage[Animal](new Crocodile)
   // i don't want a cage for cat ot contain a crocodile !
 
   // contravariant positions
-  // class CovariantVariableCage[+T](var animal: T)  // error: COVARIANT type T occurs in CONTRAVARIANT position
-  // because types of vars are in CONTRAVARIANT POSITION
+  // class CovariantVariableCage[+T](var animal: T) // error: COVARIANT type T occurs in CONTRAVARIANT position
+  // because types of vars are here in CONTRAVARIANT POSITION
 
   // without this compilation error, it would be possible to write:
   // val animalCage: CovariantCage[Animal] = new CovariantCage[Cat](new Cat)
   // animalCage.animal = new Crocodile
 
   // class ContravariantVariableCage[-T](var animal: T) // error: CONTRAVARIANT type T occurs in COVARIANT position
-  // also in COVARIANT POSITION
+  // type of vars are in COVARIANT POSITION
   // without this compilation error, it would be possible to write:
   // val catCage: ContravariantVariableCage[Cat] = new ContravariantVariableCage[Animal](new Crocodile)
 
@@ -62,9 +62,9 @@ object AnimalVariance extends App {
 
   // express the fact that a cage of cat or dog is a subtype of a cage of animal
   class AnotherCovariantCage[+T] {
-      // def addAnimal(animal: T) = true // error: COVARIANT type T occurs in CONTRAVARIANT position
+    // def addAnimal(animal: T) = true // error: COVARIANT type T occurs in CONTRAVARIANT position
   }
-  // also in CONTRAVARIANT POSITION
+  // methods arguments are in CONTRAVARIANT POSITION !
 
   // without this compilation error, it would be possible to write:
   // val anotherCCage: AnotherCovariantCage[Animal] = new AnotherCovariantCage[Dog]
@@ -72,7 +72,7 @@ object AnimalVariance extends App {
 
   // express the fact that a cage of animal is a subtype of a cage of cat
   class AnotherContravariantCage[-T] {
-    def addAnimal(animal: T) = true
+    def addAnimal(animal: T) = true // ok
   }
 
   // so we can use a cage of animal to capture a cat or a subtype of cat
@@ -82,21 +82,63 @@ object AnimalVariance extends App {
   class Kitty extends Cat
   anotherXCage.addAnimal(new Kitty)
 
-  //</editor-fold>
+  // </editor-fold>
 
-  //<editor-fold desc="Solution: Widening the types">
+  // <editor-fold desc="Creating Covariant collections by widening the types">
 
   class MyList[+A] {
     def add[B >: A](element: B): MyList[B] = new MyList[B] // widening the type
   }
 
-  val emptyList = new MyList[Kitty]
-  val animals: MyList[Kitty] = emptyList.add(new Kitty)
-  val moreAnimals: MyList[Cat] = animals.add(new Cat)
+  val emptyList                       = new MyList[Kitty]
+  val animals: MyList[Kitty]          = emptyList.add(new Kitty)
+  val moreAnimals: MyList[Cat]        = animals.add(new Cat)
   val evenMoreAnimals: MyList[Animal] = moreAnimals.add(new Dog)
 
-  //</editor-fold>
+  // </editor-fold>
 
+  // <editor-fold desc="Return types">
 
+  // trait PetShop[-T] {
+    // def get(isItAPuppy: Boolean): T // error: CONTRAVARIANT type T occurs in COVARIANT position
+  //}
 
+  // METHOD RETURN TYPES ARE IN COVARIANT POSITION
+
+  // without this compilation error, it would be possible to write:
+  // val catShop: PetShop[Animal] = new PetShop[Animal] {
+  //   override def get(isItAPuppy: Boolean): Animal = new Cat
+  // }
+
+  // val dogShop: PetShop[Dog] = catShop
+  // dogShop.get(true)  // gives me a cat !
+
+  // </editor-fold>
+
+  // <editor-fold desc="Solution: return a subtype ">
+
+  class PetShop[-T] {
+    def get[S <: T](isItAPuppy: Boolean, defaultAnimal: S): S = defaultAnimal
+  }
+
+  val shop: PetShop[Dog] = new PetShop[Animal]
+  val evilCat = shop.get(true, new Cat)
+  // if we compile: inferred type arguments [fr.brl.tips.variance.AnimalVariance.Cat]
+  // do not conform to method get's type parameter bounds [S <: fr.brl.tips.variance.AnimalVariance.Dog]
+
+  class TerraNova extends Dog
+  val bigFurry = shop.get(true, new TerraNova) // ok
+
+  /*
+  BIG RULES:
+  - method argument are in CONTRAVARIANT position
+  - return types are in COVARIANT position
+   */
+
+  // examples:
+  // trait Function1[@specialized(Specializable.Arg) -T1, @specialized(Specializable.Return) +R]
+  // Functions are contravariant in their arguments, covariant in their return type
+
+  // best explanation ever:
+  // https://twitter.com/lukaseder/status/686917793472753665
 }
